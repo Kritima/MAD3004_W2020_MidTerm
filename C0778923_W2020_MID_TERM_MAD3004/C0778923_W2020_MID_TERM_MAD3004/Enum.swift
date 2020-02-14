5b32df9adb2fa42b1fc4ff4496e743f66d2c657b
kritima <kritimakukreja5@gmail.com>
2020-02-13 20:47:29 -0500

    [KRITIMA] Enum added for error handling

---
+++ C0778923_W2020_MID_TERM_MAD3004/C0778923_W2020_MID_TERM_MAD3004/Enum.swift
@@ -0,0 +1,13 @@
+//
+//  Enum.swift
+//  C0778923_W2020_MID_TERM_MAD3004
+//
+//  Created by Kritima Kukreja on 2020-02-13.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import Foundation
+
+enum MobileError: Error{
+    case Invalid(String)
+}
